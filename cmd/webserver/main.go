fc2ff7c605ef909a254c1f9c9e4a5fd67fe856fb
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-12-29 21:40:42 +0800

    feat: add a .http file
    for a simple restful requests to test the behaviour of changes

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -35,9 +35,9 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
 	fmt.Printf("listening at %s\n", DEFAULT_PORT)
 	log.Fatal(http.ListenAndServe(DEFAULT_PORT, mux))
