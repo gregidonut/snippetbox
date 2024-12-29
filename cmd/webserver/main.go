80503053b00c7aa2615bd096c659a714ce987799
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-12-29 19:14:50 +0800

    init: intialize project
    with just 3 basic routes for now, just to understand the system for
    dynamic routes

---
+++ cmd/webserver/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const (
+	DEFAULT_PORT = ":4000"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	w.Write([]byte("hello from home"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id <= 0 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("hello from snippetView page %d\n", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	w.Write([]byte("hello from snippetcreate page"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	fmt.Printf("listening at %s\n", DEFAULT_PORT)
+	log.Fatal(http.ListenAndServe(DEFAULT_PORT, mux))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gregidonut/snippetbox
+
+go 1.22.5
