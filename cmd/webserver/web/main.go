b521d701e641d067dba93736faecc4f986dea0a9
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-12-31 03:58:22 +0800

    feat: add some flags to determine configuration
    just for the sake of doing it even though this app might not really go
    anywhere or the basic cli flags system that is supported, i really need
    to get back into viper

--- cmd/webserver/web/main.go
+++ cmd/webserver/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,14 +9,24 @@ import (
 	"github.com/gregidonut/snippetbox/cmd/webserver/web/handlers"
 )
 
-const (
-	DEFAULT_PORT = ":4000"
-)
+type config struct {
+	port          int
+	staticDirPath string
+}
 
 func main() {
+	cfg := config{
+		port:          4000,
+		staticDirPath: "./cmd/webserver/ui/static",
+	}
+
+	flag.IntVar(&cfg.port, "p", cfg.port, "HTTP port address")
+	flag.StringVar(&cfg.staticDirPath, "sdp", cfg.staticDirPath, "HTTP port address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./cmd/webserver/ui/static"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDirPath))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", handlers.Home)
@@ -23,6 +34,7 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", handlers.SnippetCreate)
 	mux.HandleFunc("POST /snippet/create", handlers.SnippetCreatePost)
 
-	fmt.Printf("listening at %s\n", DEFAULT_PORT)
-	log.Fatal(http.ListenAndServe(DEFAULT_PORT, mux))
+	port := fmt.Sprintf(":%d", cfg.port)
+	fmt.Printf("listening at %s\n", port)
+	log.Fatal(http.ListenAndServe(port, mux))
 }
