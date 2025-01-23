f3c4add5a7d99d6ae242d2dc4559407e171fe6ec
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-01-24 07:43:27 +0800

    feat: add the signup.tmpl.html
    which contains pretty much exactly the same form as the snippetCreateForm
    renamed the css to be a little more agnostic sounding
    and added prettmuch the same logic in the corresponding handler

--- cmd/webserver/web/handlers/userSignup.go
+++ cmd/webserver/web/handlers/userSignup.go
@@ -1,16 +1,17 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gregidonut/snippetbox/cmd/webserver/web/application"
+	"github.com/gregidonut/snippetbox/cmd/webserver/web/templatedata"
 )
 
 func userSignup(app *application.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		app.Debug("running userSignup handler...")
 		defer app.Debug("finished running userSignup handler")
-		fmt.Fprintf(w, "Authenticate and log the user in...")
+		data := templatedata.NewTemplateData[*templatedata.UserSignupFormData](r, app)
+		render(app, w, r, http.StatusOK, "signup", data)
 	}
 }
