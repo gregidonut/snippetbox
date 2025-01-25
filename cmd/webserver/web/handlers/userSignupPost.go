22f36f64978f52ccf47f95aeecd774abb9c4a9eb
Gregory Osilaja <gregosilaja@outlook.com>
2025-01-25 16:21:49 +0000

    feat: implement the userSignupPost handler logic
    pretty straight forward since this really just copies the analogous
    snippetCreatePost logic
    
    from the validator to the html although this doesn't yet contain the
    actual sql to insert stuff from the database as well as the happy case
    where the form is submitted validly
    
    fix: had to add the actual innerText for the error labels i forgot to
    actually do this for some reason

--- cmd/webserver/web/handlers/userSignupPost.go
+++ cmd/webserver/web/handlers/userSignupPost.go
@@ -5,12 +5,30 @@ import (
 	"net/http"
 
 	"github.com/gregidonut/snippetbox/cmd/webserver/web/application"
+	"github.com/gregidonut/snippetbox/cmd/webserver/web/templatedata"
 )
 
 func userSignupPost(app *application.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		app.Debug("running userSignupPost handler...")
 		defer app.Debug("finished running userSignupPost handler")
-		fmt.Fprintf(w, "Create a new user...")
+
+		form := &templatedata.UserSignupFormData{}
+
+		if err := app.DecodePostForm(r, &form); err != nil {
+			app.Error(err.Error())
+			app.ClientError(w, http.StatusBadRequest)
+			return
+		}
+
+		form.Validate()
+		if !form.Valid() {
+			data := templatedata.NewTemplateData[*templatedata.UserSignupFormData](r, app)
+			data.Form = form
+			render(app, w, r, http.StatusUnprocessableEntity, "signup", data)
+			return
+		}
+
+		fmt.Fprintf(w, "Create new user....")
 	}
 }
--- cmd/webserver/web/templatedata/userSignupFormData.go
+++ cmd/webserver/web/templatedata/userSignupFormData.go
@@ -9,6 +9,46 @@ type UserSignupFormData struct {
 	validator.Validator `form:"-"`
 }
 
+func (f *UserSignupFormData) GetName() string {
+	return f.Name
+}
+
+func (f *UserSignupFormData) GetEmail() string {
+	return f.Email
+}
+
+func (f *UserSignupFormData) GetPassword() string {
+	return f.Password
+}
+
 func (f *UserSignupFormData) GetValidator() validator.Validator {
 	return f.Validator
+}
+
+func (f *UserSignupFormData) Validate() {
+	f.CheckField(
+		validator.NotBlank(f.GetName()),
+		"name",
+		"This field cannot be blank",
+	)
+	f.CheckField(
+		validator.NotBlank(f.GetEmail()),
+		"email",
+		"This field cannot be blank",
+	)
+	f.CheckField(
+		validator.Matches(f.GetEmail(), validator.EmailRX),
+		"email",
+		"This field must be a vaild email address",
+	)
+	f.CheckField(
+		validator.NotBlank(f.GetPassword()),
+		"password",
+		"This field cannot be blank",
+	)
+	f.CheckField(
+		validator.MoreThanMinChars(f.GetPassword(), 8),
+		"password",
+		"field must be at least 8 characters long",
+	)
 }
