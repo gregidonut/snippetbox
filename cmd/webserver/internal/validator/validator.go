c9d4d65ab75254375bc985034597fb2300d36c58
Gregory Osilaja <gregosilaja@outlook.com>
2025-01-31 05:40:19 +0000

    feat: add a new string slice field in the validator struct
    to be used for the invalid credentials error

--- cmd/webserver/internal/validator/validator.go
+++ cmd/webserver/internal/validator/validator.go
@@ -10,18 +10,25 @@ import (
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	if v.NonFieldErrors == nil {
+		v.NonFieldErrors = []string{}
+	}
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
 		v.FieldErrors = map[string]string{}
 	}
-
 	if _, ok := v.FieldErrors[key]; !ok {
 		v.FieldErrors[key] = message
 	}
